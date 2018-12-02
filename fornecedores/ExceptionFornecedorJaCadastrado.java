36ff6ab9320bebaf0d1269faf1a20e4cd4228262
Matheus Isidoro <43383727+FUNKYBAT@users.noreply.github.com>
2018-12-02 16:00:57 -0300

    Delete ExceptionFornecedorJaCadastrado.java

--- fornecedores/ExceptionFornecedorJaCadastrado.java
+++
@@ -1,7 +0,0 @@
-package fornecedores;
-
-public class ExceptionFJC extends Exception {
-    public ExceptionFJC() {
-        super("Um fornecedor com esse nome ja existe no nosso sistema.");
-    }
-}
